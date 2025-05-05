707bfb274ebeb1385704bced354bacaf6be8069f
Ilaria <maffiolailaria1@gmail.com>
2025-05-05 16:18:23 +0200

    Refactor Header component and update styles in index.css for improved layout and navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,41 @@
 const Header = () => {
   return (
-    <header>
+    <header className="container">
         <figure>
             <img src="/src/assets/img/dc-logo.png" alt="logo" />
         </figure>
+        <ul>
+            <li>
+                <a href="#">CHARACTERS</a>
+            </li>
+            <li>
+                <a href="#">COMICS</a>
+            </li>
+            <li>
+                <a href="#">MOVIES</a>
+            </li>
+            <li>
+                <a href="#">TV</a>
+            </li>
+            <li>
+                <a href="#">GAMES</a>
+            </li>
+            <li>
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li>
+                <a href="#">VIDEOS</a>
+            </li>
+            <li>
+                <a href="#">FANS</a>
+            </li>
+            <li>
+                <a href="#">NEWS</a>
+            </li>
+            <li>
+                <a href="#">SHOP</a>
+            </li>
+        </ul>
     </header>
   )
 }
