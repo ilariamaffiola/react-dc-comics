96166bbccb8bcba97a162990e24089cafc792cea
Ilaria <maffiolailaria1@gmail.com>
2025-05-05 16:29:09 +0200

    Add Main component and update styles in index.css for layout enhancement

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+
+const Main = () => {
+  return (
+    <main>
+        <div className="container">
+            <h3>--&gt; Content goes here &lt;--</h3>
+        </div>
+    </main>
+  )
+}
+
+export default Main
