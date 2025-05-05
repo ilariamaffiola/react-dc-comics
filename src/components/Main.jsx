b989a19a20bd357eacccca5a65fb5e1c8964a1da
Ilaria <maffiolailaria1@gmail.com>
2025-05-05 17:09:48 +0200

    Enhance Main component layout with new sections and styles in index.css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,8 +2,67 @@
 const Main = () => {
   return (
     <main>
-        <div className="container">
-            <h3>--&gt; Content goes here &lt;--</h3>
+        <div className="bg-black">
+            <div className="container">
+                <h3>--&gt; Content goes here &lt;--</h3>
+            </div>
+        </div>
+        <div className="bg-light-blue">
+            <div className="container">
+                <ul>
+                    <li>
+                        <div>
+                            <img className="digital-comics" src="/src/assets/img/buy-comics-digital-comics.png" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">
+                                <span>DIGITAL COMICS</span>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img src="/src/assets/img/buy-comics-merchandise.png" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">
+                                <span>DC MERCHAMNDISE</span>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img src="/src/assets/img/buy-comics-subscriptions.png" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">
+                                <span>SUBSCRIPTION</span>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img className="shop-locator" src="/src/assets/img/buy-comics-shop-locator.png" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">
+                                <span>COMIC SHOP LOCATOR</span>
+                            </a>
+                        </div>
+                    </li>
+                    <li>
+                        <div>
+                        <img src="/src/assets/img/buy-dc-power-visa.svg" alt="" />
+                        </div>
+                        <div>
+                            <a href="#">
+                                <span>DC POWER VISA</span>
+                            </a>
+                        </div>
+                    </li>
+
+                </ul>
+            </div>
         </div>
     </main>
   )
