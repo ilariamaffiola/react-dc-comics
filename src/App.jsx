d1367f307ab182f589b92a3e51f854af12bd7173
Ilaria <maffiolailaria1@gmail.com>
2025-05-05 18:13:18 +0200

    Add Footer component and update App.jsx and index.css for layout enhancement

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import Footer from './components/Footer.jsx'
 function App() {
   return (
     <>
     <Header />
     <Main />
+    <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,106 @@
+const Footer = () => {
+  return (
+    <footer>
+        <div className="container top-footer">
+            <div className="list">
+                <ul className="dc-comics ">
+                    <li className="top-list">DC COMICS</li>
+                    <li>
+                        <a href="#">Characters</a>
+                    </li>
+                    <li>
+                        <a href="#">Comics</a>
+                    </li>
+                    <li>
+                        <a href="#">Movies</a>
+                    </li>
+                    <li>
+                        <a href="#">TV</a>
+                    </li>
+                    <li>
+                        <a href="#">Games</a>
+                    </li>
+                    <li>
+                        <a href="#">Videos</a>
+                    </li>
+                    <li>
+                        <a href="#">News</a>
+                    </li>
+                    <li className="top-list">SHOP</li>
+                    <li>
+                        <a href="#">Shop DC</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop DC Collectibles</a>
+                    </li>
+                </ul>
+                <ul className="dc">
+                    <li className="top-list">DC</li>
+                    <li>
+                        <a href="#">Terms Of Use</a>
+                    </li>
+                    <li>
+                        <a href="#">Privacy policy (New)</a>
+                    </li>
+                    <li>
+                        <a href="#">Ad Choices</a>
+                    </li>
+                    <li>
+                        <a href="#">Advertising</a>
+                    </li>
+                    <li>
+                        <a href="#">Jobs</a>
+                    </li>
+                    <li>
+                        <a href="#">Subscriptions</a>
+                    </li>
+                    <li>
+                        <a href="#">Talent Workshops</a>
+                    </li>
+                    <li>
+                        <a href="#">CPSC Certificates</a>
+                    </li>
+                    <li>
+                        <a href="#">Ratings</a>
+                    </li>
+                    <li>
+                        <a href="#">Shop Help</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact Us</a>
+                    </li>
+                </ul>
+                <ul className="sites">
+                    <li className="top-list">SITES</li>
+                    <li>
+                        <a href="#">DC</a>
+                    </li>
+                    <li>
+                        <a href="#">MAD Magazine</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Kids</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Universe</a>
+                    </li>
+                    <li>
+                        <a href="#">DC Power Visa</a>
+                    </li>
+
+                </ul>
+            </div>
+            <div className="logo-footer">
+                <img src="/src/assets/img/dc-logo-bg.png" alt="#
+                " />
+            </div>
+        </div>
+        <div className="footer-nav">
+            <div className="sign-up"></div>
+            <div className="social"></div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
